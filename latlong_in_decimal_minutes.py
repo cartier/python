b1c8a760f8b1aa826c3e87341e0d11dfb41648d8
Brian Cartier <brian.cartier@gmail.com>
2020-06-22 11:11:13 -0400

    latlong - separate out utility from package

--- latlong_in_decimal_minutes.py
+++ latlong_in_decimal_minutes.py
@@ -2,16 +2,10 @@
 
 
 """Print lat/long in DD MM.MMM format given decimal degrees"""
-
-import re,sys,os
+import sys
 
 def printf(format, *args):
     sys.stdout.write(format % args)
-
-
-def usage():
-  print "\nUsage :" + os.path.basename(sys.argv[0]) + " xx.xxxxxx, yy.yyyyyy\n"
-  sys.exit(1)
 
 def latlong_in_decimal_minutes(lat, lon):
         lat = float(lat)
@@ -37,16 +31,3 @@ def latlong_in_decimal_minutes(lat, lon):
 
         printf("\n%c %2d %02.3f %c %03d %02.3f\n", cardinal_lat, latdeg, latmin, cardinal_lon, londeg, lonmin)
 
-
-if(len(sys.argv) == 1):
-    usage()
-    exit
-
-input=' '.join(sys.argv[1:])
-
-try:
-    (lat,lon) =  [t(s) for t,s in zip((float,float),re.search('^([-\d.]+)[,\s]+([-\d.]+)$',input).groups())]
-    latlong_in_decimal_minutes(lat, lon)
-
-except:
-    usage()
